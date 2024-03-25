b63866d82bf14f2d648297ece367a118ed3beefb
IM-GYURI <kgulr0517@gmail.com>
2024-03-25 21:07:48 +0900

    Spring boot와 Java를 활용하여 Account(계좌 관리) 시스템을 만드는 프로젝트

---
+++ src/main/java/com/example/account/exception/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.example.account.exception;
+
+import com.example.account.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.example.account.type.ErrorCode.INTERNET_SERVER_ERROR;
+import static com.example.account.type.ErrorCode.INVALID_REQUEST;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AccountException.class)
+    public ErrorResponse handleAccountException(AccountException e) {
+        log.error("{} is occured.", e.getErrorCode());
+
+        return new ErrorResponse(e.getErrorCode(), e.getErrorMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException is occured.");
+
+        return new ErrorResponse(INVALID_REQUEST, INVALID_REQUEST.getDescription());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        log.error("DataIntegrityViolationException is occured.");
+
+        return new ErrorResponse(INVALID_REQUEST, INVALID_REQUEST.getDescription());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ErrorResponse handleException(Exception e) {
+        log.error("Exception is occured.");
+
+        return new ErrorResponse(INTERNET_SERVER_ERROR, INTERNET_SERVER_ERROR.getDescription());
+    }
+}
